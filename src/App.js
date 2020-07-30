0cdb623676f5df4c56d2d0e04b4b7dc2ad640859
Jimmy Bull <jimmybull18@gmail.com>
2020-07-30 08:23:23 -0400

    Changed function component to class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from "react";
-
+import Todolist from './components/Todolist';
+import Todoitem from './components/Todoitem';
+import Todoform from './components/Todoform'
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <h1>Welcome to glitchy-planner. :)</h1>
-      <h2>What are your plans today?</h2>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor() {
+      super();
+      this.state = {
+        todos: []
+
+      };
+    }
+  
+    render() {
+      return(
+        <div>
+          <Todoform/>
+        </div>
+      );
+  }
+  componentDidMount = () => {
+    const todos = localStorage.getItem('todos');
+    if(todos) {
+      const savedTodos = JSON.parse(todos);
+      this.setState({ todos: savedTodos });
+    } else {
+      console.log( 'No todos');
+    }
+  }
+
 }
 
 export default App;
