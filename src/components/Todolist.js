064d0f1cf9cfb04fa4e24067d96d1eac57a9653c
Jimmy Bull <jimmybull18@gmail.com>
2020-07-30 08:25:40 -0400

    Added class component and imported to app component

---
+++ src/components/Todolist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Todolist extends React.Component {
+    render() {
+        return(
+            <div>
+                Hello from the Todolist component.
+            </div>
+        );
+    }
+
+}
+export default Todolist;
