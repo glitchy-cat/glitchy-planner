d71922f109cfbfdb305b50774c7fa6e354bffe3c
Jimmy Bull <jimmybull18@gmail.com>
2020-07-30 09:44:10 -0400

    Added addtodo function that stores items into local storage

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import React from "react";
-import Todolist from './components/Todolist';
-import Todoitem from './components/Todoitem';
-import Todoform from './components/Todoform'
+import Todolist from "./components/Todolist";
+import Todoitem from "./components/Todoitem";
+import Todoform from "./components/Todoform";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    };
+  }
 
-    constructor() {
-      super();
-      this.state = {
-        todos: []
-
-      };
-    }
-  
-    render() {
-      return(
-        <div>
-          <Todoform/>
-        </div>
-      );
+  render() {
+    return (
+      <div>
+        <Todoform addTodoFn={this.addTodo}/>
+        <Todolist todos={this.state.todos}/>
+      </div>
+    );
   }
   componentDidMount = () => {
-    const todos = localStorage.getItem('todos');
-    if(todos) {
+    const todos = localStorage.getItem("todos");
+    if (todos) {
       const savedTodos = JSON.parse(todos);
       this.setState({ todos: savedTodos });
     } else {
-      console.log( 'No todos');
+      console.log("No todos");
     }
-  }
+  };
 
+  addTodo = async (todo) => {
+    await this.setState({ todos: [...this.state.todos, todo] });
+    localStorage.setItem("todos", JSON.stringify(this.state.todos));
+    console.log(localStorage.getItem('todos'));
+  };
 }
 
 export default App;
