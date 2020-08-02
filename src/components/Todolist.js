af9bf7066633f9e6fa0bf422297c8d30b4e05f00
Jimmy Bull <jimmybull18@gmail.com>
2020-08-01 20:00:44 -0400

    Created class component to render todo list items

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
 class Todolist extends React.Component {
-    render() {
-        return(
-            <div>
-                Hello from the Todolist component.
-            </div>
-        );
-    }
+  render() {
+    const { todos } = this.props;
 
+    return (
+      <div className="todoListContainer">
+        {todos.map((_todo, _index) => {
+          return <div key={_index}>{_todo}</div>;
+        })}
+      </div>
+    );
+  }
 }
 export default Todolist;
