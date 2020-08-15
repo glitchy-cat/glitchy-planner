cc8c8ef8420de9245287d78f255372ead1113f7b
Jimmy Bull <jimmybull18@gmail.com>
2020-08-15 12:09:09 -0400

    Working on unfinished edit functionality

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -3,17 +3,39 @@ import  trash from "../icons/trash.svg";
 import '../styles/todostyle.css';
 
 class Todoitem extends React.Component {
+    constructor() {
+      super();
+      this.state = { editing: false}
+    }
+
   render() {
     const { todo } = this.props;
 
     return (
       <div  className="todoItem" key={this.props._index}>
-        {todo.text}
+        {
+          !this.state.editing && (
+           <div onDoubleClick={() => this.setState({ editing: true}) }>{todo.text}</div>
+          )
+        }
         <img className="trashBin" src={trash} alt="" onClick={this.deleteTodo}></img>
-        <button type="button" className="btn btn-primary" onClick={this.toggleTodo} >Complete</button>
+        <button type="button" className="btn btn-primary" onClick={this.toggleTodo} >Complete</button>        
+      {
+        this.state.editing && (
+        <form onSubmit={() => this.setState({ editing: false}) }>
+        <input type="text"
+         onChange={ (e) => {
+           console.log(e.target.value)
+            todo.text = e.target.value  //unifinished. changing on console log but not changing on input field
+         }}
+          value={todo.text} />
+          <input type="submit" value="submit" />
+          </form>)
+      }
       </div>
     );
   }
+  
 
   toggleTodo = () => {
     this.props.updateTodoFn(this.props.todo);
