86bff18ee9bce4bcd96a3d0f82de768be49c7ae3
Jimmy Bull <jimmybull18@gmail.com>
2020-08-27 18:54:58 -0400

    Updates

---
+++ precache-manifest.f479777df27f75c094ae7914c6755b4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ffa9512f6ad7b318c169c26ecb57e1",
+    "url": "/glitchy-planner/index.html"
+  },
+  {
+    "revision": "8ddf3ddd4fd92aa0e775",
+    "url": "/glitchy-planner/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f1e94acb5a8e3b8375fe",
+    "url": "/glitchy-planner/static/css/main.abf6fc1c.chunk.css"
+  },
+  {
+    "revision": "8ddf3ddd4fd92aa0e775",
+    "url": "/glitchy-planner/static/js/2.a2ca6c76.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/glitchy-planner/static/js/2.a2ca6c76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e94acb5a8e3b8375fe",
+    "url": "/glitchy-planner/static/js/main.85f9fc35.chunk.js"
+  },
+  {
+    "revision": "948962e79ebee71fa0d1",
+    "url": "/glitchy-planner/static/js/runtime-main.522bd577.js"
+  }
+]);
